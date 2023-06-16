7fd8c0e4eb2f2a682fd6c533871ccce224cc2b03
Hichammayat <hichammayat07@gmail.com>
2023-06-16 11:13:24 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+  *main - entry main
+  *Descriptin: Numberz
+  *Return: always 0
+  */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + 48);
+	}
+	putchar('\n');
+	return (0);
+}
