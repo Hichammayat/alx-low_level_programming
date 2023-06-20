dec3b047e7aea8c9f2473c7b136776024df3796f
Hichammayat <hichammayat07@gmail.com>
2023-06-20 08:24:01 -0700

    commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,16 @@
 #include "main.h"
 /**
-  *main - entry point
+  *print_alphabet - entry point
   *Description: the alphabet, in lowercase
   *Return: always with 0
-*/
+  */
 void print_alphabet(void)
 {
 	char l;
-	for (l = 'a' ; l <= 'z' ; l++ )
+
+	for (l = 'a' ; l <= 'z' ; l++)
 	{
 		_putchar(l);
 	}
 	_putchar('\n');
 }
-
-	
