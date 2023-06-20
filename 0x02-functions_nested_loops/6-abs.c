d739d353b236f9d3f34f39fe54a577bcdc2ffbfd
Hichammayat <hichammayat07@gmail.com>
2023-06-20 10:54:30 -0700

    commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  *_abs - computes the absolute value of an integer01
+  *@i: input number
+  *Return: absolute num
+  */
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+
+}
