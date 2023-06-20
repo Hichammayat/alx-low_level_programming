22e107d9ac06788d5211fe83da312164d2538597
Hichammayat <hichammayat07@gmail.com>
2023-06-20 08:14:52 -0700

    commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  *main - entry point
+  *Description: the alphabet, in lowercase
+  *Return: always with 0
+*/
+void print_alphabet(void)
+{
+	char l;
+	for (l = 'a' ; l <= 'z' ; l++ )
+	{
+		_putchar(l);
+	}
+	_putchar('\n');
+}
+
+	
