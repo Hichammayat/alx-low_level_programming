bb67a38fb1ae2b045b674eda5ec5b30c6879a909
Hichammayat <hichammayat07@gmail.com>
2023-06-20 15:27:34 -0700

    commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+  *add - adds two integers and returns the result
+  *@a : number
+  *@b : number
+  *Return: the result addition
+  */
+int add(int a, int b)
+{
+	return (a + b);
+}
