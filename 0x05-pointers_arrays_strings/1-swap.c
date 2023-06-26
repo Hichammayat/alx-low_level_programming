efa336f66d95595ef58ab0d205ddfd97ef7c4cdd
Hichammayat <hichammayat07@gmail.com>
2023-06-26 09:21:48 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - swaps the values of two integers
+ * @a: integer to swap
+ * @b: integer to swap
+ */
+void swap_int(int *a, int *b)
+{
+	int tm;
+
+	tm = *a;
+	*a = *b;
+	*b = tm;
+}
+
