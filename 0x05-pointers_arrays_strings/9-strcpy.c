4258560deeb6b42d1291d468bcb8ad6f14620192
Hichammayat <hichammayat07@gmail.com>
2023-06-26 10:06:21 -0700

    commit

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,18 +8,14 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int A = 0;
-	int C = 0;
+	int i = -1;
 
-	while (*(src + A) != '\0')
-	{
-		A++;
-	}
-	for ( ; C < A ; C++)
-	{
-		dest[C] = src[C];
-	}
-	dest[A] = '\0';
+	do {
+		i++;
+		dest[i] = src[i];
+	} while (src[i] != '\0');
 	return (dest);
 }
+
+
 
