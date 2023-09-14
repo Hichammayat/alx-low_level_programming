2e84b271caa7184e4dc373cfbc2a3dbf994556f6
Hichammayat <hichammayat07@gmail.com>
2023-09-14 13:46:44 -0700

    commit

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,19 +8,19 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	const dlistint_t *temp;
-	int count = 0;
+	const dlistint_t *tmp;
+	int num = 0;
 
-	temp = h;
+	tmp = h;
 
 	if (h == NULL)
 		return (0);
 
-	while (temp)
+	while (tmp)
 	{
-		printf("%d\n", temp->n);
-		count++;
-		temp = temp->next;
+		printf("%d\n", tmp->n);
+		num++;
+		tmp = tmp->next;
 	}
-	return (count);
+	return (num);
 }
